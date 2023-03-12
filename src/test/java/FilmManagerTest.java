9ffbf3e184afcb901dbb434914e5111d70492250
YanaK <Yana.Koval15@yandex.ru>
2023-03-12 14:01:31 +0300

    main

---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void test (){
+        FilmManager manager = new FilmManager();
+        manager.add( "Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+
+        String[] expected = { "Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void shoudFindLastFilms(){
+        FilmManager manager = new FilmManager();
+        manager.add( "Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        String[] expected = {"Film 3","Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+}
