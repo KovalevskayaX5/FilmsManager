75fa0b5859dfc6bc2e18adeb4bf6ca91df64d43d
YanaK <Yana.Koval15@yandex.ru>
2023-03-27 20:20:38 +0300

    main

--- src/main/java/FilmManager.java
+++ src/main/java/FilmManager.java
@@ -2,12 +2,14 @@ public class FilmManager {
     private String[] films = new String[0];
 
     private int limit;
-    public FilmManager(){
+
+    public FilmManager() {
         this.limit = 10;
     }
-     public FilmManager (int limit){
+
+    public FilmManager(int limit) {
         this.limit = limit;
-     }
+    }
 
     public void add(String film) {
         String[] tmp = new String[films.length + 1];
@@ -19,23 +21,23 @@ public class FilmManager {
 
     }
 
-     public String[] findAll() {
+    public String[] findAll() {
         return films;
 
-     }
+    }
 
     public String[] findLast() {
         int resultLength;
-        if (films.length<limit){
+        if (films.length < limit) {
             resultLength = films.length;
-        }else {
-            resultLength= limit;
+        } else {
+            resultLength = limit;
         }
         String[] result = new String[resultLength];
-        for (int i =0; i< result.length; i++){
-            result[i] = films[films.length -1 -i];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = films[films.length - 1 - i];
 
         }
         return result;
-     }
+    }
 }
--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -4,24 +4,24 @@ import org.junit.jupiter.api.Test;
 public class FilmManagerTest {
 
     @Test
-    public void test (){
+    public void test() {
         FilmManager manager = new FilmManager();
-        manager.add( "Film 1");
+        manager.add("Film 1");
         manager.add("Film 2");
         manager.add("Film 3");
 
-        String[] expected = { "Film 1", "Film 2", "Film 3"};
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
         String[] actual = manager.findAll();
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shoudFindLastFilms(){
+    public void shoudFindLastFilms() {
         FilmManager manager = new FilmManager();
-        manager.add( "Film 1");
+        manager.add("Film 1");
         manager.add("Film 2");
         manager.add("Film 3");
-        String[] expected = {"Film 3","Film 2", "Film 1"};
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
 
